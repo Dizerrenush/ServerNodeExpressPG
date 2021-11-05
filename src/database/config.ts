77c4daf695f583934fadc368974736e305bb7454
DiZeRRenush <dima555zhernov@gmial.com>
2021-11-05 03:05:29 -0700

    sequilize ts

--- src/database/config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "qwerty1234",
-    database: "demo_db",
-    dialect: "postgres"
-};
--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from 'sequelize'
-import { dbConfig } from "../database/config"
-
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-});
